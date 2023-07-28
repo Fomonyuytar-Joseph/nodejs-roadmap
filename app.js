0016875e57e308d81e5d5535744aac1d9858b5b0
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-07-28 21:37:16 +0100

    params

--- app.js
+++ app.js
@@ -1,56 +1,69 @@
-const fs = require('fs'); 
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
-
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
-
   res.status(200).json({
     status: 'success',
     results: tours.length,
-    data:{
-      tours:tours
-    }
+    data: {
+      tours: tours,
+    },
+  });
+});
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
 
-  })
+  const id = req.params.id * 1;
 
+  if(id > tours.length){
+    return res.status(404).json({
+      status:"failied",
+      message:"invalid ID"
+    })
+  }
 
-  app.post('/api/v1/tours', (req, res)=>{
-    const newId = tours[tours.length - 1].id + 1;
-    //merge two existing objects together
-    const newTour= Object.assign({id:newId},req.body)
+  const tour = tours.find((el) => el.id === id);
 
-    tours.push(newTour);
+  res.status(200).json({
+    status: 'success',
 
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err =>{
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  //merge two existing objects together
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
       res.status(201).json({
         status: 'success',
-        data:{
-          tour:newTour
-        }
-      })
-    });
-
-  })
-
-
-
-
-  // res.status(200).json({
-  //   message: 'Hello from server',
-  //   app: 'natours',
-  // });
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
-app.post('/', (req, res) => {
-  res.send('You can send to this end point');
-});
+// app.post('/', (req, res) => {
+//   res.send('You can send to this end point');
+// });
 
 const port = 8000;
 app.listen(port, () => {
