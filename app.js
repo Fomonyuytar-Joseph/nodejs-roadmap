549623c15c08e2366d51a5f5bf19d36491e6d8fd
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-07-29 09:18:38 +0100

    middle ware

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+
+})
+
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
