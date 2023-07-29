775fa3e24aa7fd00e1f3d30e39c81633b92bebcc
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-07-29 10:32:43 +0100

    controllers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan')
+
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
 const app = express();
 
@@ -8,159 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'))
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-
-})
-
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-
-//route handlers
-
-const getAllTours = (req, res) => {
-
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-
-  const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'failied',
-      message: 'invalid ID',
-    });
-  }
-
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-
-    data: {
-      tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  //merge two existing objects together
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'failied',
-      message: 'invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<update tour here ....>',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'failied',
-      message: 'invalid ID',
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: "this route is under build"
-  })
-}
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: "this route is under build"
-  })
-}
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: "this route is under build"
-  })
-}
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: "this route is under build"
-  })
-}
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: "this route is under build"
-  })
-}
-
-
 
 //routes
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-
-
-
-
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
-
-
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-const port = 8000;
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
-});
+module.exports = app
---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+
+
+
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "this route is under build"
+    })
+}
+exports.getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "this route is under build"
+    })
+}
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "this route is under build"
+    })
+}
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "this route is under build"
+    })
+}
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "this route is under build"
+    })
+}
