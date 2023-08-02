a0775727647c9976493096cd3276bf139cfd6df9
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-08-02 14:32:18 +0100

    'eslint'

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -10,15 +10,13 @@ const app = express();
 app.use(express.json());
 
 if(process.env.NODE_ENV === 'development'){
-app.use(morgan('dev'));
+ app.use(morgan('dev'));
 }
-
 
 app.use(express.static(`${__dirname}/public`));
 
-
 //routes
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-module.exports = app
+module.exports = app;
