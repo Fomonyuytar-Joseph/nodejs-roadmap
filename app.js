de82874cad62ffbfd8e56d503eb3f6ec28afca40
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-08-14 08:23:49 +0100

    error handling

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+
+const AppError= require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -18,5 +21,18 @@ app.use(express.static(`${__dirname}/public`));
 //routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+
+app.all('*',(req,res,next)=>{
+  
+    // const err = new Error(`cant find ${req.originalUrl}`);
+    // err.status='fail';
+    // err.statusCode=404;
+
+    next(new AppError(`cant find ${req.originalUrl}`,404));
+})
+
+
+app.use(globalErrorHandler)
 
 module.exports = app;
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Tour = require('./../models/toursModel');
-
+const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError')
 // const tours = JSON.parse(
 //     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
@@ -31,11 +32,11 @@ const Tour = require('./../models/toursModel');
 //     next();
 // }
 
-exports.getAllTours = async (req, res) => {
+exports.getAllTours = catchAsync(async (req, res,next) => {
 
-    console.log(req.query);
+    
 
-    try{
+    // try{
       //build query
       const queryObj = { ...req.query };
       const excludedFields = ['page', 'sort', 'limit', 'fields'];
@@ -52,35 +53,12 @@ exports.getAllTours = async (req, res) => {
     
 
       // { duration: { gte: '5' }, difficulty: 'easy' }
-      let query = Tour.find(JSON.parse(queryStr) );
+      let query = Tour.find( );
 
 
-      //2)sorting
-      if(req.query.sort){
-        const sortBy = req.query.sort.split(',').join('')
-             query= query.sort(sortBy)
-             //sort('price ratingAverge')
-      }else{
-        query= query.sort('-createdAt');
-      }
+     
 
-
-    //3)field limiting 
-    if(req.query.fields){
-        const fields =  req.query.fields.split(',').join('');
-
-        query= query.select(fields)
-    }else{
-        query= query.select('-__v')
-    }
-
-
-    //4)Pagnination
-    const page = req.query.page *1 || 1;
-    const limit = req.query.limit* 1 || 100
-    const skip = (page - 1)*1|| 100
-    //page=2&limit=10 1-10 page1 11-20 page2
-    query= query.skip(skip).limit(limit)
+    
 
 
       //execute the query
@@ -95,18 +73,22 @@ exports.getAllTours = async (req, res) => {
           tours,
         },
       });
-    }catch(err){
-    res.status(404).json({
-        status:"fail",
-        message:'cannot get all tours'
-    })
-    }
-};
+    // }catch(err){
+    // res.status(404).json({
+    //     status:"fail",
+    //     message:'cannot get all tours'
+    // })
+    // }
+});
 
-exports.getTour = async (req, res) => {
+exports.getTour = catchAsync(async (req, res,next) => {
+  
 
-    try{
+    // try{
        const tour= await Tour.findById(req.params.id)
+       if(!tour){
+       return  next( new AppError('No tour with that id found',404))
+       }
 
        res.status(200).json({
         status: 'success',
@@ -115,24 +97,27 @@ exports.getTour = async (req, res) => {
         }
        })
 
-    }catch(err){
+    // }catch(err){
 
-        res.status(404).json({
-            status: 'fail',
-            message:'tour not found'
-        })
+    //     res.status(404).json({
+    //         status: 'fail',
+    //         message:'tour not found'
+    //     })
 
-    }
+    // }
     
-};
+});
 
-exports.createTour = async (req, res) => {
+
+
+
+exports.createTour = catchAsync( async (req, res, next) => {
 
     //  const newTour = newTour({})
 
     //  newTour.save();
 
-    try{
+    // try{
           const newTour = await Tour.create(req.body);
           res.status(200).json({
             status: 'success',
@@ -141,14 +126,14 @@ exports.createTour = async (req, res) => {
             },
           });
 
-    }catch(err){
+    // }catch(err){
 
-        res.status(400).json({
-            status: 'fail',
-            message:err
-        })
-        // console.log(err);
-    }
+    //     res.status(400).json({
+    //         status: 'fail',
+    //         message:err
+    //     })
+    //     // console.log(err);
+    // }
 
 
   
@@ -175,11 +160,11 @@ exports.createTour = async (req, res) => {
     //         });
     //     }
     // );
-};
+});
 
-exports.updateTour = async (req, res) => {
+exports.updateTour = catchAsync(async (req, res , next) => {
 
-    try{
+    // try{
      const tour = await Tour.findByIdAndUpdate(req.params.id,req.body,{
         new:true,
         runValidators:true
@@ -191,35 +176,39 @@ exports.updateTour = async (req, res) => {
             tour
         }
      })
-    }catch(err){
-          res.status(404).json({
-            status: 'fail',
-            message:'Invalid request'
-        })
+    // }catch(err){
+    //       res.status(404).json({
+    //         status: 'fail',
+    //         message:'Invalid request'
+    //     })
 
-    }
-};
+    // }
+});
 
-exports.deleteTour = async (req, res) => {
-    try{
-         await  Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = catchAsync(async (req, res ,next) => {
+    // try{
+       const tour =  await  Tour.findByIdAndDelete(req.params.id);
+
+          if (!tour) {
+            return next(new AppError('No tour with that id found', 404));
+          }
         res.status(204).json({
             status:"success",
              data:null
         })
 
-    }catch(err){
-         res.status(400).json({
-           status: 'fail',
-           message: 'Invalid request',
-         });
+    // }catch(err){
+    //      res.status(400).json({
+    //        status: 'fail',
+    //        message: 'Invalid request',
+    //      });
 
-    }
+    // }
    
 
     // res.status(204).json({
     //     status: 'success',
     //     data: null,
     // });
-};
+});
 
